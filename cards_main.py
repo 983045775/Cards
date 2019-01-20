eea255c5d167ab73fcc1f7129cd393b324a0a9d1
liucheng <983045775@qq.com>
2019-01-20 15:21:28 +0800

    self的运用

--- cards_main.py
+++ cards_main.py
@@ -1,6 +1,5 @@
 #! /Library/Frameworks/Python.framework/Versions/3.7/bin/python3
 
-import cards_constant
 from cards_tools import CardsUtils
 
 cards_utils = CardsUtils()
@@ -13,19 +12,19 @@ while True:
     print("您选择的操作是:%s" % choose)
     print("-" * 50)
 
-    if choose in [cards_constant.S_ADD, cards_constant.S_SHOW_ALL, cards_constant.S_SEACH]:
-        if choose.__eq__(cards_constant.S_ADD):
+    if choose in [cards_utils.S_ADD, cards_utils.S_SHOW_ALL, cards_utils.S_SEACH]:
+        if choose.__eq__(cards_utils.S_ADD):
             # 添加
             cards_utils.add_cards()
-        elif choose.__eq__(cards_constant.S_SHOW_ALL):
+        elif choose.__eq__(cards_utils.S_SHOW_ALL):
             # 查看所有
             cards_utils.show_all()
             pass
-        elif choose.__eq__(cards_constant.S_SEACH):
+        elif choose.__eq__(cards_utils.S_SEACH):
             # 搜索
             cards_utils.seach_name()
             pass
-    elif choose.__eq__(cards_constant.S_EXIT):
+    elif choose.__eq__(cards_utils.S_EXIT):
         print("欢迎再次使用名片系统！")
         print("-" * 50)
         break
--- cards_tools.py
+++ cards_tools.py
@@ -1,7 +1,16 @@
-import cards_constant
-
 
 class CardsUtils:
+    # 三种选项
+
+    S_ADD = "1"
+    S_SHOW_ALL = "2"
+    S_SEACH = "3"
+    S_EXIT = "0"
+
+    str_space = "\t\t\t\t"
+
+    # 存储个人信息的列表
+    cards_list = []
 
     def splash_welcome(self):
         """欢迎界面
@@ -9,10 +18,10 @@ class CardsUtils:
         """
         print("*" * 50)
         print("欢迎使用【名片管理系统】V2.0.0", end="\r\n\r\n")
-        print("%s.新增名片" % cards_constant.S_ADD)
-        print("%s.显示全部" % cards_constant.S_SHOW_ALL)
-        print("%s.搜索名片" % cards_constant.S_SEACH, end="\r\n\r\n")
-        print("%s.退出系统" % cards_constant.S_EXIT)
+        print("%s.新增名片" % self.S_ADD)
+        print("%s.显示全部" % self.S_SHOW_ALL)
+        print("%s.搜索名片" % self.S_SEACH, end="\r\n\r\n")
+        print("%s.退出系统" % self.S_EXIT)
         print("*" * 50)
 
     def add_cards(self):
@@ -28,24 +37,24 @@ class CardsUtils:
                         "mail": mail_str}
             print(msg_dict)
             print("新增 %s 的名片成功！" % name_str)
-            cards_constant.cards_list.append(msg_dict)
+            self.cards_list.append(msg_dict)
         else:
             print("不要输入空")
 
     def show_all(self):
-        if len(cards_constant.cards_list) == 0:
+        if len(self.cards_list) == 0:
             print("没有人哦")
             return
         print("显示所有名片")
         print(
-            "姓名" + cards_constant.str_space + "电话" + cards_constant.str_space + "QQ" + cards_constant.str_space + "邮箱")
+            "姓名" + self.str_space + "电话" + self.str_space + "QQ" + self.str_space + "邮箱")
         print("=" * 50)
         # 遍历所有列表字典
         i = 1
-        for msg_dict in cards_constant.cards_list:
+        for msg_dict in self.cards_list:
             i += 1
             for key, value in msg_dict.items():
-                print("%s" % value, end=cards_constant.str_space)
+                print("%s" % value, end=self.str_space)
             if i != (len(msg_dict) - 1):
                 print()
 
@@ -54,18 +63,60 @@ class CardsUtils:
 
         seach_name = input("请输入您要查找的姓名：")
 
-        for msg_dict in cards_constant.cards_list:
+        for msg_dict in self.cards_list:
             # 字典对象
             if msg_dict["name"].__eq__(seach_name):
                 # 说明找到了
                 print(
-                    "姓名" + cards_constant.str_space + "电话" + cards_constant.str_space + "QQ" + cards_constant.str_space + "邮箱")
+                    "姓名" + self.str_space + "电话" + self.str_space + "QQ" + self.str_space + "邮箱")
                 print("==" * 50)
                 for key, value in msg_dict.items():
-                    print("%s" % value, end=cards_constant.str_space)
+                    print("%s" % value, end=self.str_space)
                 # TODO 新的选项
                 print()
-                cards_constant.del_or_change(msg_dict["name"])
+                self.del_or_change(msg_dict["name"])
                 break
         else:
             print("您要查找的姓名不存在，请确认！")
+
+    def del_or_change(self, name):
+        while True:
+            choose = input("请输入您想要的操作（[1]、修改；[2]、删除；[0]、返回上级菜单）：")
+            if choose.__eq__("1"):
+                # 修改
+                for dict_people in self.cards_list:
+                    if dict_people["name"].__eq__(name):
+                        # 找到了
+                        self.change_dict(dict_people)
+                break
+            elif choose.__eq__("2"):
+                # 删除
+                for dict_people in self.cards_list:
+                    if dict_people["name"].__eq__(name):
+                        self.cards_list.remove(dict_people)
+                        print("删除成功")
+                break
+            elif choose.__eq__("0"):
+                # 退出
+                break
+            else:
+                print("输入错误,请重来")
+
+    def change_dict(self, dict_msg):
+        name_str = input("修改姓名(不修改则输入回车)").strip()
+        phone_str = input("修改电话号码(不修改则输入回车)").strip()
+        qq_str = input("修改QQ号码(不修改则输入回车)").strip()
+        mail_str = input("修改邮箱地址(不修改则输入回车)").strip()
+
+        if len(name_str) == 0:
+            name_str = dict_msg["name"]
+        if len(phone_str) == 0:
+            phone_str = dict_msg["phone"]
+        if len(qq_str) == 0:
+            qq_str = dict_msg["qq"]
+        if len(mail_str) == 0:
+            mail_str = dict_msg["mail"]
+        dict_msg.update({"name": name_str})
+        dict_msg.update({"qq": qq_str})
+        dict_msg.update({"mail": mail_str})
+        dict_msg.update({"phone": phone_str})
